c8db81e31820c7bae09543f6e77d95d1cedf3f08
Marcel Rödder <skribinto@gmail.com>
2019-05-25 21:43:19 +0000

    Tutorial 7 abgeschlossen

--- django_tutorial/django_tutorial/urls.py
+++ django_tutorial/django_tutorial/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'), # mit name kann im Tamplate die Seite angesprochen werden: {% register %}
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), # template_name ist der Ort, an dem das Tamplate gesucht werden soll
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), # auth_views.LogoutView.as_view() ist die django-eigene Logout-Seite
     path('', include('blog.urls')),
 ]
